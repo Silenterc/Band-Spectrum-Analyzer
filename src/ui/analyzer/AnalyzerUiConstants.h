f4e86b420755bf6fbec53904a8cc4ac6fde48de3
Lukas Zima <54321lukas@seznam.cz>
2026-03-28 20:47:03 +0100

    Changed polling to be 60 fps

--- src/ui/analyzer/AnalyzerUiConstants.h
+++ src/ui/analyzer/AnalyzerUiConstants.h
@@ -9,7 +9,7 @@ namespace Ui::AnalyzerConstants {
     inline constexpr float defaultVisibleMinFrequencyHz = Analyzer::Constants::minFrequencyHz;
     inline constexpr float defaultVisibleMaxFrequencyHz = maxUiFrequencyHz;
 
-    inline constexpr int meterPollIntervalMs = 33; // ~30 Hz
+    inline constexpr int meterPollIntervalMs = 16; // ~60 Hz
     inline constexpr int idleMeterPollIntervalMs = 120; // ~8 Hz while waiting for signal to return
     inline constexpr float meterPollIntervalSeconds = static_cast<float>(meterPollIntervalMs) * 0.001f;
 
